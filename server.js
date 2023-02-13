6f44fd20f1d6a1bb3dd5943e423470e06a245502
AbuBakr1770 <zaroon3310@gmail.com>
2023-02-13 18:38:11 +0500

    put require app after dotenv.config

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const app = require('./app')
 const dotenv = require('dotenv')
 
 dotenv.config({path:'./config.env'})
+const app = require('./app')
 
 const port =process.env.PORT;
 app.listen(port, () => {
