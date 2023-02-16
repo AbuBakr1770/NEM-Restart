b8956429a9a1c4127c6d86d31077838e79091b64
AbuBakr1770 <zaroon3310@gmail.com>
2023-02-16 12:11:53 +0500

    updated tour Model

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,22 +1,63 @@
-const mongoose = require('mongoose')
-
+const mongoose = require('mongoose');
 
 const TourSchema = new mongoose.Schema({
-    name:{
+  name: {
+    type: String,
+    unique: true,
+    required: [true, 'A tour must a name'],
+  },
+  duration: {
+    type: Number,
+    required: [true, 'A tour must have a duration'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour Must have Max Group Size'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'a tour must have a difficulty'],
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must a price'],
+  },
+  ratingAverage: {
+    type: Number,
+    default: 4.0,
+  },
+  ratingQuantity: {
+    type: Number,
+    default: 0,
+  },
+  discount: Number,
+  summary: {
+    type: String,
+    trim: true,
+    required:[true,'tour must have a summary']
+  },
+  description:{
+    type:String,
+    trim:true // trim reamver all the extra white spaces in a string
+  },
+  imageCover:{
+    type:String,
+    required:{
       type:String,
-      unique:true,
-      required:[true,'A tour must a name']
-    },
-    price:{
-      type:Number,
-      required:[true,'A tour must a price']
-    },
-    rating:{
-      type:Number,
-      required:[true,'4.5']
+      required:[true,'tour must have a cover image']
     }
-  })
-  
-  TourModal = mongoose.model('ToueModal', TourSchema)
+  },
+  images:{
+    type:[String]  // it means the type will be an array of strings e.g type:[String]
+  },
+  createdAt:{
+    type:Date,
+    default: Date.now(),
+  },
+  startDates:[Date],
 
-  module.exports = TourModal
+});
+
+TourModal = mongoose.model('ToueModal', TourSchema);
+
+module.exports = TourModal;
